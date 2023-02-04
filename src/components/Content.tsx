957f34d174c5e5a50acaa44576e77eac1c13becc
Aearsears <53102185+Aearsears@users.noreply.github.com>
2023-02-04 17:59:21 -0500

    add array state

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,5 +1,5 @@
 import { Button } from 'antd';
-import React from 'react';
+import React, { useState } from 'react';
 import Array from './Array';
 
 interface AppProps {
@@ -7,14 +7,18 @@ interface AppProps {
 }
 
 const MyContent: React.FC<AppProps> = ({ data }: AppProps) => {
-    const array1 = [54, 23, 90];
-    const array2 = [89, 23, 1];
+    const [array1, setArray1] = useState([54, 23, 90]);
+    const [array2, setArray2] = useState([89, 23, 1]);
+
     const swapValues = () => {
         let x1 = array1[0];
         array1[0] = array2[0];
         array2[0] = x1;
+        setArray1([...array1]);
+        setArray2([...array2]);
         console.log('swapped');
     };
+
     return (
         <div>
             <Array data={array1}></Array>
