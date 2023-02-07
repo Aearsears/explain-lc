131a84f1a4e5856aa2cda1d162307882cb6610d5
Aearsears <53102185+Aearsears@users.noreply.github.com>
2023-02-07 00:28:28 -0500

    added animations

--- src/components/Array.tsx
+++ src/components/Array.tsx
@@ -1,12 +1,16 @@
 import { Avatar, Badge } from 'antd';
 import React from 'react';
+import { useAutoAnimate } from '@formkit/auto-animate/react';
 
 interface AppProps {
     data: number[];
+    ref: React.Ref<HTMLButtonElement>;
 }
 
-const Array: React.FC<AppProps> = ({ data }: AppProps) => {
+const Array: React.FC<AppProps> = ({ data }: AppProps & {}) => {
     const cells = [];
+
+    const [parent, enableAnimations] = useAutoAnimate(/* optional config */);
     for (let i = 0; i < data.length; i++) {
         cells.push(
             <Badge>
@@ -16,7 +20,7 @@ const Array: React.FC<AppProps> = ({ data }: AppProps) => {
             </Badge>
         );
     }
-    return <div>{cells}</div>;
+    return <ul ref={parent}>{cells}</ul>;
 };
 
 export default Array;
