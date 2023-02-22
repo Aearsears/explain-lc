83ba56c60dbac9fedf5b8dc3c2ac1ca4b6c043c1
Aearsears <53102185+Aearsears@users.noreply.github.com>
2023-02-21 22:43:26 -0500

    smooth transitions with framer

--- src/components/Array.tsx
+++ src/components/Array.tsx
@@ -1,5 +1,5 @@
 import { Avatar, Badge, Button } from 'antd';
-import { motion } from 'framer-motion';
+import { motion, spring } from 'framer-motion';
 import React from 'react';
 
 interface ArrayProps {
@@ -38,43 +38,51 @@ class Array extends React.Component<ArrayProps, ArrayState> {
 
     dict: { [id: number]: React.ReactElement } = {};
 
-    // componentDidMount() {
-    //     const cells: React.ReactElement[] = [];
-    //     for (let i = 0; i < this.props.data.length; i++) {
-    //         let element = (
-    //             <Cell
-    //                 data={this.state.cellProps[i].data}
-    //                 x={this.state.cellProps[i].x}
-    //                 y={this.state.cellProps[i].y}
-    //                 rotation={this.state.cellProps[i].rotation}
-    //             ></Cell>
-    //         );
-    //         this.dict[i] = element;
-    //         cells.push(element);
-    //     }
-    //     this.setState({ cellProps: cellProps, cells: cells });
-    // }
+    componentDidMount() {
+        const cells: React.ReactElement[] = [];
+        for (let i = 0; i < this.props.data.length; i++) {
+            let element = (
+                <Cell
+                    data={this.state.cellProps[i].data}
+                    x={this.state.cellProps[i].x}
+                    y={this.state.cellProps[i].y}
+                    rotation={this.state.cellProps[i].rotation}
+                ></Cell>
+            );
+            this.dict[i] = element;
+            cells.push(element);
+        }
+        this.setState({ ...this.state, cells: cells });
+    }
 
     swapValues = () => {
         let cellProps = [...this.state.cellProps];
+        let cells = [...this.state.cells];
         console.log(cellProps);
 
         //swap first and second elements, 0th and 1st
-        let element1 = cellProps[0];
-        let element2 = cellProps[1];
+        let prop1 = cellProps[0];
+        let prop2 = cellProps[1];
+
+        let element1 = cells[0];
+        let element2 = cells[1];
+
         //update the cells props
-        let temp = element1.data;
-        element1.data = element2.data;
-        element2.data = temp;
+        // let temp = element1.data;
+        // element1.data = element2.data;
+        // element2.data = temp;
 
         //update pos
-        element1.x = element1.x + 50;
-        element2.x = element2.x - 50;
+        // element1.x = element1.x + 50;
+        // element2.x = element2.x - 50;
 
-        cellProps[0] = element1;
-        cellProps[1] = element2;
+        cellProps[1] = prop1;
+        cellProps[0] = prop2;
 
-        this.setState({ ...this.state, cellProps: cellProps });
+        cells[1] = element1;
+        cells[0] = element2;
+
+        this.setState({ cells, cellProps });
         console.log('swapped');
         console.log(this.state);
     };
@@ -95,8 +103,18 @@ class Array extends React.Component<ArrayProps, ArrayState> {
         }
         return (
             <div>
-                {cells}
-                <Button onClick={this.swapValues}>swap</Button>
+                <ul>
+                    {this.state.cellProps.map((cellProp) => (
+                        <Cell
+                            key={cellProp.data}
+                            data={cellProp.data}
+                            x={cellProp.x}
+                            y={cellProp.y}
+                            rotation={cellProp.rotation}
+                        ></Cell>
+                    ))}
+                    <Button onClick={this.swapValues}>swap</Button>
+                </ul>
             </div>
         );
     }
@@ -109,14 +127,14 @@ class Cell extends React.Component<CellProps, {}> {
     }
     render() {
         return (
-            <motion.div
+            <motion.li
                 className="box"
                 animate={{
                     x: this.props.x,
                     y: this.props.y,
                     rotate: this.props.rotation
                 }}
-                transition={{ type: 'just' }}
+                layout
                 style={{
                     border: '3px dotted black',
                     display: 'inline-block'
@@ -127,7 +145,8 @@ class Cell extends React.Component<CellProps, {}> {
                         {this.props.data}
                     </Avatar>
                 </Badge>
-            </motion.div>
+                test
+            </motion.li>
         );
     }
 }
